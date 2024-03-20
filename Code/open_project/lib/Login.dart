e6be8eeeab9cf3f42f571aed4a0a2f9f1dc21279
DESKTOP-2AESMQJ\Eng.Shaaban <shaaban99shahin@gmail.com>
2024-03-20 10:55:26 +0300

    Rewrite login

--- Code/open_project/lib/Login.dart
+++ Code/open_project/lib/Login.dart
@@ -2,7 +2,12 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:gradient_widgets/gradient_widgets.dart';
+import 'package:http/http.dart' as http;
 import 'package:http/http.dart';
+import 'package:open_project/DetailOfProject.dart';
+import 'package:open_project/Project.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'main.dart';
 
@@ -13,28 +18,63 @@ class LoginScreen extends StatefulWidget {
 
 class Login extends State<LoginScreen> {
   TextEditingController email = TextEditingController();
-  TextEditingController password = TextEditingController();
+  TextEditingController enteredToken = TextEditingController();
+  List<Project> data = [];
+  late String name;
+  late int id;
+  String? apikey;
+  String? token;
+  String username = 'apikey';
+  String password =
+      '6905fd9498adf5f3f7024adac280c2d45fd042622094484cc56dc77aed52773e';
 
-  void getUser() async {
-    String username = 'apikey';
-    String password =
-        '6905fd9498adf5f3f7024adac280c2d45fd042622094484cc56dc77aed52773e';
-    String basicAuth =
-        'Basic ' + base64.encode(utf8.encode('$username:$password'));
-    print(basicAuth);
+  void getData() {
+    Uri uri = Uri.parse("https://op.yaman-ka.com/api/v3/projects");
+
+    http.get(uri).then((response) {
+      var jsonResponse = jsonDecode(response.body);
+      var embedded = jsonResponse['_embedded'];
+      var elements = embedded['elements'] as List;
+
+      setState(() {
+        Iterable<Project> projects =
+            elements.map((data) => Project(id: data['id'], name: data['name']));
+        data = projects.toList();
+      });
+    });
+  }
+
+  void getProjects() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    await prefs.setString('apikey', username);
+    await prefs.setString('password', password);
+
+    apikey = prefs.getString('apikey');
+    token = prefs.getString('password');
+    String basicAuth = 'Basic ${base64.encode(utf8.encode('$apikey:$token'))}';
 
     Response r = await get(Uri.parse('https://op.yaman-ka.com/api/v3/projects'),
         headers: <String, String>{'authorization': basicAuth});
-    print(r.statusCode);
-    print(r.body);
-    Fluttertoast.showToast(msg: 'Done...');
+    if (r.statusCode == 200) {
+      var jsonResponse = jsonDecode(r.body);
+      var embedded = jsonResponse['_embedded'];
+      var elements = embedded['elements'] as List;
+      setState(() {
+        Iterable<Project> projects =
+            elements.map((data) => Project(id: data['id'], name: data['name']));
+        data = projects.toList();
+      });
+    } else {
+      Fluttertoast.showToast(msg: 'You dont sign up');
+    }
   }
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    getUser();
+    getData();
   }
 
   @override
@@ -42,52 +82,104 @@ class Login extends State<LoginScreen> {
     // TODO: implement build
     return Scaffold(
       appBar: AppBar(
-        title: Center(child: Text("Welcome to back")),
+        backgroundColor: Colors.blueAccent,
+        title: const Text("Login",
+            style: TextStyle(fontSize: 25, color: Colors.white)),
       ),
-      body: Column(children: [
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: TextField(
-            controller: email,
-            keyboardType: TextInputType.emailAddress,
-            decoration: const InputDecoration(
-              prefixIcon: Icon(Icons.email),
-              labelText: "Enter Email",
-              border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(
-                Radius.circular(15),
-              )),
-              //isDense: true,
+      body: Column(
+        children: [
+          const Image(
+            image: AssetImage('images/openproject.png'),
+            width: 125,
+            height: 125,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: email,
+              keyboardType: TextInputType.emailAddress,
+              decoration: const InputDecoration(
+                prefixIcon: Icon(Icons.email),
+                labelText: "Enter Apikey",
+              ),
             ),
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: TextField(
-            controller: password,
-            keyboardType: TextInputType.visiblePassword,
-            decoration: const InputDecoration(
-              prefixIcon: Icon(Icons.password),
-              labelText: "Enter Password",
-              border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(
-                Radius.circular(15),
-              )),
-              //isDense: true,
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: enteredToken,
+              keyboardType: TextInputType.visiblePassword,
+              decoration: InputDecoration(
+                prefixIcon: const Icon(Icons.token),
+                suffixIcon: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.remove_red_eye_sharp),
+                ),
+                labelText: "Enter Token",
+              ),
             ),
           ),
-        ),
-        ElevatedButton(
-          onPressed: () {
-            setState(() {});
-          },
-          style: button(),
-          child: Text(
-            'Login now...',
-            style: TextStyle(fontSize: 25),
+          Align(
+            alignment: Alignment.centerRight,
+            child: TextButton(
+                onPressed: () {}, child: const Text('Forgetpassword?')),
           ),
-        ),
-      ]),
+          GradientButton(
+            increaseWidthBy: 250,
+            increaseHeightBy: 10,
+            child: Text('Login',
+                style: TextStyle(fontSize: 25, color: Colors.white)),
+            callback: () {
+              Fluttertoast.showToast(msg: 'Done');
+            },
+            gradient:
+                LinearGradient(colors: [Colors.blue, Colors.purpleAccent]),
+            shadowColor: Gradients.backToFuture.colors.last.withOpacity(0.25),
+          ),
+          /*ElevatedButton(
+            onPressed: () {
+              setState(() {});
+            },
+            style: button(),
+            child: Text(
+              'Login now...',
+              style: TextStyle(fontSize: 25),
+            ),
+          ),*/
+          Expanded(
+              child: ListView.builder(
+            shrinkWrap: true,
+            scrollDirection: Axis.vertical,
+            padding: const EdgeInsets.all(20.0),
+            itemCount: data.length,
+            itemBuilder: (BuildContext ctx, int index) {
+              name = data[index].name;
+              id = data[index].id;
+              return ListTile(
+                  title: Text(name),
+                  subtitle: Text(id.toString()),
+                  //tileColor: Color.fromARGB(255, 146, 105, 105),
+                  trailing: IconButton(
+                    icon: const Icon(Icons.delete),
+                    color: Colors.red,
+                    onPressed: () {
+                      setState(() {
+                        data.removeAt(index);
+                      });
+                    },
+                  ),
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) =>
+                                StateDetail(data[index].id, data[index].name)));
+                    //Fluttertoast.showToast(msg: data[index].name);
+                  });
+            },
+          )),
+        ],
+      ),
     );
   }
 }
