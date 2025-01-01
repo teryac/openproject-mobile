680a38928fe36d9643094095b3568af4aa45272e
DESKTOP-2AESMQJ\Eng.Shaaban <shaaban99shahin@gmail.com>
2025-01-01 19:37:50 +0300

    try adding status

--- Code/open_project/lib/Subjects.dart
+++ Code/open_project/lib/Subjects.dart
@@ -2,14 +2,22 @@ class Subjects {
   int id;
   String subject;
   var description;
+  //String status;
 
   Subjects(
-      {required this.id, required this.subject, required this.description});
+      {required this.id,
+      required this.subject,
+      required this.description /*,
+      required this.status*/
+      });
 
   static fromJson(Map<String, dynamic> parsedJson) {
     var description = parsedJson['description'];
     var raw = description['raw'];
     return Subjects(
-        id: parsedJson['id'], subject: parsedJson['subject'], description: raw);
+      id: parsedJson['id'],
+      subject: parsedJson['subject'],
+      description: raw, /* status: parsedJson['status']*/
+    );
   }
 }
