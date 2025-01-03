482fb110be75923c2487d135f0ae0313a6fbc578
DESKTOP-2AESMQJ\Eng.Shaaban <shaaban99shahin@gmail.com>
2025-01-03 15:07:00 +0300

    fix code

--- Code/open_project/lib/Login.dart
+++ Code/open_project/lib/Login.dart
@@ -7,6 +7,8 @@ import 'package:open_project/DetailOfProject.dart';
 import 'package:open_project/Project.dart';
 import 'package:open_project/main.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -28,7 +30,7 @@ class Login extends State<LoginScreen> {
   String password =
       '6905fd9498adf5f3f7024adac280c2d45fd042622094484cc56dc77aed52773e';
 
-  /* void getData() {
+  void getData() {
     Uri uri = Uri.parse("https://op.yaman-ka.com/api/v3/projects");
 
     http.get(uri).then((response) {
@@ -36,13 +38,22 @@ class Login extends State<LoginScreen> {
       var embedded = jsonResponse['_embedded'];
       var elements = embedded['elements'] as List;
 
+// Loop through the elements to extract 'description' -> 'raw'
+      List<Project> projects = elements.map((data) {
+        // Safely access the raw field
+        String rawDescription =
+            data['description']?['raw'] ?? 'No description available';
+        String name = data['name'];
+        id = data['id'];
+
+        return Project(description: rawDescription, name: name, id: id);
+      }).toList();
+
       setState(() {
-        Iterable<Project> projects =
-            elements.map((data) => Project(id: data['id'], name: data['name']));
-        data = projects.toList();
+        data = projects;
       });
     });
-  }*/
+  }
 
   void getProjects() async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
@@ -57,16 +68,6 @@ class Login extends State<LoginScreen> {
     Response r = await get(Uri.parse('https://op.yaman-ka.com/api/v3/projects'),
         headers: <String, String>{'authorization': basicAuth});
     if (r.statusCode == 200) {
-      /*var jsonResponse = jsonDecode(r.body);
-      var embedded = jsonResponse['_embedded'];
-      var elements = embedded['elements'] as List;
-      setState(() {
-        //var des = elements['description'];
-        Iterable<Project> projects = elements.map((data) =>
-            Project(description: data['description'], name: data['name']));
-        data = projects.toList();
-
-      });*/
       var jsonResponse = jsonDecode(r.body);
 
       // Access the '_embedded' object
@@ -96,14 +97,13 @@ class Login extends State<LoginScreen> {
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
+    //getData();
     getProjects();
   }
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
       backgroundColor: const Color(0xfff8f8f8),
       appBar: AppBar(
@@ -119,123 +119,92 @@ class Login extends State<LoginScreen> {
         title: const Text("List of projects",
             style: TextStyle(fontSize: 25, color: Colors.white)),
       ),
-      body: Column(
-        children: [
-          /*const Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Image(
-              image: AssetImage('images/openproject.png'),
-              width: 175,
-              height: 175,
-            ),
-          ),
-          Container(
-            margin: const EdgeInsets.only(left: 15.0, right: 15.0),
-            child: TextField(
-              controller: apiKey,
-              keyboardType: TextInputType.emailAddress,
-              decoration: const InputDecoration(
-                prefixIcon: Icon(Icons.email),
-                labelText: "Enter Apikey",
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 15.0, right: 15.0),
-            child: TextField(
-              controller: enteredToken,
-              keyboardType: TextInputType.visiblePassword,
-              decoration: InputDecoration(
-                prefixIcon: const Icon(Icons.token),
-                suffixIcon: IconButton(
-                  color: Colors.grey,
-                  onPressed: () {},
-                  icon: const Icon(Icons.remove_red_eye_sharp),
+      body: data.isEmpty
+          ? const Center(child: Text('No projects available...'))
+          : Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ///const SizedBox(height: 15.0),
+                const Padding(
+                  padding: EdgeInsets.only(top: 8.0, left: 8.0),
+                  child: Text(
+                    "Hi Shaaban",
+                    style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.cyan,
+                        fontWeight: FontWeight.bold),
+                  ),
                 ),
-                labelText: "Enter Token",
-              ),
-            ),
-          ),
-          Align(
-            alignment: Alignment.centerRight,
-            child: TextButton(
-                onPressed: () {},
-                child: const Text('Forgot password?',
-                    style: TextStyle(fontSize: 15))),
-          ),
-          GradientButton(
-            increaseWidthBy: 250,
-            increaseHeightBy: 10,
-            child: const Text('Login',
-                style: TextStyle(fontSize: 20, color: Colors.white)),
-            callback: () {
-              if (apiKey.text == username && enteredToken.text == password) {
-                getProjects();
-                apiKey.text = '';
-                enteredToken.text = '';
-              } else {
-                Fluttertoast.showToast(msg: 'Enter values correctly please');
-              }
-            },
-            gradient: const LinearGradient(
-                colors: [Colors.blueAccent, Colors.purpleAccent]),
-            shadowColor: Gradients.backToFuture.colors.last.withOpacity(0.75),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              setState(() {});
-            },
-            style: button(),
-            child: Text(
-              'Login now...',
-              style: TextStyle(fontSize: 25),
-            ),
-          ),*/
-          Expanded(
-              child: ListView.builder(
-            //shrinkWrap: true,
-            scrollDirection: Axis.vertical,
-            padding: const EdgeInsets.all(12.0),
-            itemCount: data.length,
-            itemBuilder: (BuildContext ctx, int index) {
-              name = data[index].name;
-              description = data[index].description;
+                const Padding(
+                  padding: EdgeInsets.only(
+                    left: 8.0,
+                  ),
+                  child: Text(
+                    "Welcome back",
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(top: 12.0, left: 8.0),
+                  child: Text(
+                    "Projects:",
+                    style: TextStyle(
+                        fontSize: 15,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Expanded(
+                    child: ListView.builder(
+                  //shrinkWrap: true,
+                  scrollDirection: Axis.vertical,
+                  padding: const EdgeInsets.all(8.0),
+                  itemCount: data.length,
+                  itemBuilder: (BuildContext ctx, int index) {
+                    name = data[index].name;
+                    description = data[index].description;
 
-              id = data[index].id;
-              return Card(
-                  borderOnForeground: true,
-                  elevation: 3.0,
-                  child: Column(
-                    //mainAxisSize: MainAxisSize.max,
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      ListTile(
-                          title: Text(name),
-                          subtitle: Text(description.toString()),
-                          //tileColor: Color.fromARGB(255, 146, 105, 105),
-                          trailing: IconButton(
+                    id = data[index].id;
+                    return Card(
+                        borderOnForeground: true,
+                        color: Colors.white,
+                        elevation: 3.0,
+                        child: Column(
+                          //mainAxisSize: MainAxisSize.max,
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            ListTile(
+                                title: Text(
+                                  name,
+                                  style: const TextStyle(
+                                      fontSize: 15.0,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                subtitle: Text(description),
+                                /*trailing: IconButton(
                             icon: const Icon(Icons.delete),
-                            color: Colors.red,
+                            //color: Colors.red,
                             onPressed: () {
                               setState(() {
                                 data.removeAt(index);
                               });
                             },
-                          ),
-                          onTap: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => StateDetail(
-                                        data[index].id, data[index].name)));
-                            //Fluttertoast.showToast(msg: data[index].name);
-                          })
-                    ],
-                  ));
-            },
-          )),
-        ],
-      ),
+                          ),*/
+                                onTap: () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => StateDetail(
+                                              data[index].id,
+                                              data[index].name)));
+                                  //Fluttertoast.showToast(msg: data[index].name);
+                                })
+                          ],
+                        ));
+                  },
+                )),
+              ],
+            ),
     );
   }
 }
