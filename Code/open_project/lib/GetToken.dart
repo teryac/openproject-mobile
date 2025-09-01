6efce82d71b5612d675514219f20530c765db463
DESKTOP-2AESMQJ\Eng.Shaaban <shaaban99shahin@gmail.com>
2025-09-01 16:14:34 +0300

    Complete code

--- Code/open_project/lib/GetToken.dart
+++ Code/open_project/lib/GetToken.dart
@@ -1,5 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:http/http.dart';
 import 'package:open_project/GetServer.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class GetToken extends StatefulWidget {
   const GetToken({super.key});
@@ -23,11 +28,44 @@ ButtonStyle buttonToken() {
 }
 
 class Token extends State<GetToken> {
-  String username = 'apikey';
-  String password =
+  String apiKey = 'apikey';
+  String token =
       '6905fd9498adf5f3f7024adac280c2d45fd042622094484cc56dc77aed52773e';
   TextEditingController enteredToken = TextEditingController();
   String? error;
+
+  void checkToken() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    await prefs.setString('apikey', apiKey);
+
+    String basicAuth = 'Basic ${base64.encode(utf8.encode('$apiKey:$token'))}';
+
+    Response r = await get(Uri.parse('https://op.yaman-ka.com/api/v3/projects'),
+        headers: <String, String>{'authorization': basicAuth});
+    if (r.statusCode == 200) {
+      await prefs.setString('password', enteredToken.text);
+      /*var jsonResponse = jsonDecode(r.body);
+      var embedded = jsonResponse['_embedded'];
+      var elements = embedded['elements'] as List;
+      setState(() {
+        Iterable<Project> projects = elements.map((data) => Project(
+            description: data['description'],
+            name: data['name'],
+            id: data['id']));
+        data = projects.toList();
+      });*/
+      Fluttertoast.showToast(
+          msg: 'Done :)',
+          textColor: Colors.white,
+          backgroundColor: Colors.green);
+    } else {
+      Fluttertoast.showToast(
+          msg: 'You dont sign up',
+          textColor: Colors.white,
+          backgroundColor: Colors.red);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -71,9 +109,7 @@ class Token extends State<GetToken> {
                 style: TextStyle(fontSize: 16, color: Colors.black54),
               ),
             ),
-            const SizedBox(
-              height: 15,
-            ),
+            const SizedBox(height: 15),
             const Padding(
               padding: EdgeInsets.only(left: 20.0),
               child: Align(
@@ -193,34 +229,16 @@ class Token extends State<GetToken> {
               child: ElevatedButton(
                 style: buttonServer(),
                 onPressed: () {
-                  //enteredServer.text = "";
-                  /*if (enteredServer.text.isEmpty) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(
-                        backgroundColor: Colors.green,
-                        duration: Duration(milliseconds: 500),
-                        content: Text(
-                          "Login successful",
-                          style: TextStyle(fontSize: 20, color: Colors.white),
-                        ),
-                      ),
-                    );
-
+                  if (enteredToken.text.isNotEmpty) {
+                    checkToken();
+                    enteredToken.text = "";
+                    error = "";
                     setState(() {});
                   } else {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(
-                        backgroundColor: Colors.red,
-                        duration: Duration(milliseconds: 500),
-                        content: Text(
-                          "Login failed",
-                          style: TextStyle(fontSize: 20, color: Colors.white),
-                        ),
-                      ),
-                    );
+                    error = "Enter API Token.";
                     Navigator.pop(context);
                     setState(() {});
-                  }*/
+                  }
                 },
                 child: const Text(
                   "Access workspace",
