e665a062252dea12ff971fb357d81ca553db7ba3
MajdHajHmidi <majdhajhmidi@gmail.com>
2026-02-03 21:53:36 +0300

    Prepared project for Android release

--- src/android/app/build.gradle.kts
+++ src/android/app/build.gradle.kts
@@ -1,3 +1,12 @@
+import java.util.Properties
+import java.io.FileInputStream
+
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
+
 plugins {
     id("com.android.application")
     // START: FlutterFire Configuration
@@ -34,6 +43,15 @@ android {
         versionName = flutter.versionName
     }
 
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String?
+            keyPassword = keystoreProperties["keyPassword"] as String?
+            storeFile = keystoreProperties["storeFile"]?.let { file(it) }
+            storePassword = keystoreProperties["storePassword"] as String?
+        }
+    }
+    
     buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
